b99da47c0186f5e6821534e9a17ea4bab954240e
noemichis <noemi_chis@yahoo.com>
2023-01-31 19:56:15 +0000

    add loop to create question-answer pairs

--- run.py
+++ run.py
@@ -13,8 +13,38 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SPREADSHEET = GSPREAD_CLIENT.open('the_history_quiz')
 
 questions = SPREADSHEET.worksheet('questions')
+answers = SPREADSHEET.worksheet('answers')
 
 
-data = questions.get_all_values()
+question_list = questions.get_all_values()
+get_answers = answers.get_all_values()
 
-print(data)
+question_num = 1
+
+for question in question_list:
+    print(question)
+    for answer in get_answers[question_num-1]:
+        print(answer)
+
+    question_num += 1
+
+
+# for question, answer in question_list, answ:
+#     print(question, answer)
+
+# dict(zip(question_list, get_answers))
+
+# print(my_dict)
+
+# for answer_select in range(len(get_answers)):
+#     for answer in get_answers[answer_select]:
+#         print(answer, end=" ")
+#     print()
+
+# print(answer_list)
+
+# for question, answer in question_list, answer_l:
+
+#     print(f"{question}\n {answer}")
+
+# print(f"{question}\n {answer_list}")
