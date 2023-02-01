2b0ea3652a0c779a7d41a86e4965941b32aad5a0
noemichis <noemi_chis@yahoo.com>
2023-02-01 16:47:33 +0000

    add get_questions function to create q/a pairs

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import string
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,35 +18,41 @@ questions = SPREADSHEET.worksheet('questions')
 answers = SPREADSHEET.worksheet('answers')
 
 
-question_list = questions.get_all_values()
-get_answers = answers.get_all_values()
+topic1 = questions.get_all_values()
+# get_answers = answers.get_all_values()
 
-question_num = 1
-
-for question in question_list:
-    print(question)
-    for answer in get_answers[question_num-1]:
-        print(answer)
-
-    question_num += 1
+#loop through values returned and create dictionary
+questions = {}
+for select in topic1:
+    question = select[0]
+    answers = select[1:]
+    questions[question] = answers
 
 
-# for question, answer in question_list, answ:
-#     print(question, answer)
+#creates quiz
+def get_questions():
+    for num, (q, a) in enumerate(questions.items(), start=1):
+        print(f"\n{num}: {q}")
+        correct_answer = a[0]
+        sort_label = dict(zip(string.ascii_uppercase, sorted(a)))
+        for label, a in sort_label.items():
+            print(f" {label}. {a}")
 
-# dict(zip(question_list, get_answers))
+        a_label = input(f'\n Your answer: ').upper()
+        answer = sort_label[a_label]
+        if answer == correct_answer:
+            print('Correct')
+        else:
+            print('Wrong')        
 
-# print(my_dict)
 
-# for answer_select in range(len(get_answers)):
-#     for answer in get_answers[answer_select]:
-#         print(answer, end=" ")
-#     print()
+get_questions()
 
-# print(answer_list)
+# question_num = 1
+# for question in question_list:
+#     print(question)
+#     for answer in get_answers[question_num-1]:
+#         print(answer)
 
-# for question, answer in question_list, answer_l:
+#     question_num += 1
 
-#     print(f"{question}\n {answer}")
-
-# print(f"{question}\n {answer_list}")
