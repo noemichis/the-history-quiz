6bf7f08a41ec6985f12159a6a70b54bd783c83df
noemichis <noemi_chis@yahoo.com>
2023-02-04 16:45:01 +0000

    add function to push user data to Google Sheets

--- run.py
+++ run.py
@@ -27,14 +27,13 @@ for select in topic1:
     question = select[0]
     answers = select[1:]
     questions[question] = answers
-    
-user_score = []
 
 
 # creates quiz
 def get_questions():
     """
-    Loops through questions dictionary and displays the keys enumerated and the values sorted
+    Loops through questions dictionary and displays the keys enumerated
+    and the values sorted
     """
     score = 0
     for num, (q, a) in enumerate(questions.items(), start=1):
@@ -43,28 +42,31 @@ def get_questions():
         sort_label = dict(zip(string.ascii_uppercase, sorted(a)))
         for label, a in sort_label.items():
             print(f" {label}. {a}")
-
-        a_label = input(f'\n Your answer: ').upper()
+# create stand-alone function for checking score
+        a_label = input('\n Your answer: ').upper()
         answer = sort_label[a_label]
-        if answer == correct_answer:        #create stand-alone function for checking score
+        if answer == correct_answer:
             score += 1
-            print('You got the answer right')
+            print("You got the answer right")
         else:
-            print(f'Sorry, the correct answer is {correct_answer}')        
+            print(f"Sorry, the correct answer is {correct_answer}")
+
+    return score
 
 
 def start_game():
     """
-    The function will start a new quiz displaying the rules, asking for a username (and displaying topics)
+    The function will start a new quiz displaying the rules, asking
+    for a username (and displaying topics)
     """
 
-    print(f"\n Welcome to The History Quiz")
+    print("\n Welcome to The History Quiz")
 
     # get_username()
 
-    print(f"\n Do you want to start a new quiz?")
+    print("\n Do you want to start a new quiz?")
 
-    get_questions()
+    # get_questions()
 
 
 def get_username():
@@ -75,59 +77,53 @@ def get_username():
         username = input("Please choose a username: ").strip()
 
         if username == '':
-            print(f"Username must not be empty")
+            print("Username must not be empty")
         elif not len(username) > 2:
             print("Your username must contain at least 3 characters")
         else:
             print(f"Hi {username}!")
             break
+    return username
 
 
-
-        #try:
-        #     if len(username) > 2:
-        #         print(f"Hi {username}!")
-        #         break
-        # except TypeError:
-        #     print("Your username must contain at least 3 characters")
-
-# def confirm():
-#     """
-#     Confirms if the answer is valid
-#     """
-#     while True:
-#         yes_no = input(f"\n Y / N: ").upper()
-
-#         if yes_no not in ("Y", "N"):
-#             print(f'Please try again')
-#         elif yes_no == "Y":
-#             get_questions()
-#         else:
-#             break
+def update_worksheet(data, worksheet):
+    """
+    Push username and score to the leaderboard worksheet
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SPREADSHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 
 def replay():
     while True:
-        yes_no = input(f"\n Would you like to play again?\n Y / N: ").upper()
+        yes_no = input("\n Would you like to play again?\n Y / N: ").upper()
 
         if yes_no not in ("Y", "N"):
-            print(f'Please try again')
+            print("Please try again")
         elif yes_no == "Y":
             return True
         else:
             return False
 
+
 def main():
     """
     Runs the main program
     """
-    get_username()
+    username = get_username()
     start_game()
-    
+    score = get_questions()
+    data = username, score
+    user_score = [i for i in data]
+    update_worksheet(user_score, 'leaderboard')
+
     while replay():
         start_game()
 
     print('goodnight')
+
 
 main()
 
@@ -139,3 +135,9 @@ main()
 
 #     question_num += 1
 
+# try:
+#     if len(username) > 2:
+#         print(f"Hi {username}!")
+#         break
+# except TypeError:
+# print("Your username must contain at least 3 characters")
